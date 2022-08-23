c870bca423db293aa17a2a6da224a1d22a37ca44
Matibox <mateusz.hladky@gmail.com>
2022-08-23 14:55:45 +0200

    Add mobile menu functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles/main.scss';
 import assets from './assets';
 assets();
+
+import nav from './nav';
+nav();
+
+// Enable transitions after the page loads
+window.addEventListener('load', () => {
+    document.querySelectorAll('.preload').forEach(element => {
+        element.classList.remove('preload');
+    });
+});
---
+++ src/nav.js
@@ -0,0 +1,20 @@
+export default function nav() {
+    const iconActiveClass = 'header__nav-icon--active';
+    const navActiveClass = 'nav--active';
+    const overlayActiveClass = 'nav__overlay--active';
+    const status = 0;
+    const open = document.querySelector('[data-menu-open]');
+    const close = document.querySelector('[data-menu-close]');
+    const nav = document.querySelector('[data-nav]');
+    const overlay = document.querySelector('[data-overlay]');
+
+    open.addEventListener('click', toggle);
+    close.addEventListener('click', toggle);
+
+    function toggle() {
+        open.classList.toggle(iconActiveClass);
+        close.classList.toggle(iconActiveClass);
+        nav.classList.toggle(navActiveClass);
+        overlay.classList.toggle(overlayActiveClass);
+    }
+}
